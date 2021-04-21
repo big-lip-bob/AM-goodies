d4049454a9957d0729d40aa85af3f354d1beae25
big-lip-bob <41026257+big-lip-bob@users.noreply.github.com>
2021-04-21 21:52:24 +0000

    Update EventPusher.lua

--- EventHandler/EventPusher.lua
+++ EventHandler/EventPusher.lua
@@ -1,1 +1,1 @@
-require("ExperimentalEvent").push(select((...)=="event" and 2 or 1,...)) -- skip the 1st "event" argument for non key | mouse events
+require("EventHandler").push(select((...)=="event" and 2 or 1,...)) -- skip the 1st "event" argument for non key | mouse events
