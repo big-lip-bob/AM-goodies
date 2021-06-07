2bb7ce9a6122cfe4df551ae4cf5ff5d94254fc79
big-lip-bob <41026257+big-lip-bob@users.noreply.github.com>
2021-06-07 19:28:43 +0200

    Added empty check for push

--- EventHandler/EventHandler.lua
+++ EventHandler/EventHandler.lua
@@ -165,6 +165,7 @@ function event:test_filters(filter,...)
 end
 
 function event:push(...)
+	if #self.listeners == 0 then return end
 	local args = varags_pack(...) -- could be lazyly init but cba
 	self.mutex.lock()
 	for i = #self.listeners,1,-1 do
